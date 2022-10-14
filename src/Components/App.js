f6cd3115b79949e66ed3e3f4772d713a1ccbce3e
Jordan <JMCollins0@aol.com>
2022-10-14 14:41:48 -0400

    Add todo component. Restyle Links to be bottom bar
    Document title is now "React Home"
    TopThree component added to properly style links list.

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,14 @@
 import '../Styles/linkstyle.css';
 import '../Styles/App.css';
+import '../Styles/topthree.css'
 import { AllLinks } from './links.js';
 import Clock from './clock.js'
+import TopThreeTodo from './TopThree.js'
 function App() {
   return (
     <div>
       <Clock/>
+      <TopThreeTodo />
     <AllLinks />
     </div>
   );
---
+++ src/Components/TopThree.js
@@ -0,0 +1,9 @@
+function TopThreeTodo(){
+    return(
+        <div id="top-three">
+            An Empty Div
+        </div>
+    )
+}
+
+export default TopThreeTodo;
