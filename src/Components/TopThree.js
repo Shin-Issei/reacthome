be5bd8737fb805e459d9c3d2a56a67777121b109
Jordan <JMCollins0@aol.com>
2022-10-21 09:40:25 -0400

    Convert TODO component to class

--- src/Components/TopThree.js
+++ src/Components/TopThree.js
@@ -1,18 +1,18 @@
 // This is just like the old todo list app lets try not to make this too complicated...
-
+import React from "react";
 /*  The flow of the operations
 
 I - Get an input (Make an input box)
 
 
 */
-
-function TopThreeTodo(){
-    return(
-        <div id="top-three">
-            <input value="The to do list text is coming from here" ></input>
-        </div>
-    )
+class TopThreeTodo extends React.Component() {
+  render() {
+    return (
+      <div id="top-three">
+        <input value="The to do list text is coming from here"></input>
+      </div>
+    );
+  }
 }
-
 export default TopThreeTodo;
