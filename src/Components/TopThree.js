a8e18d5aeb343077651b7084b447271ca8fff26f
Jordan <JMCollins0@aol.com>
2022-10-21 09:38:47 -0400

    Add input text box to todo list component

--- src/Components/TopThree.js
+++ src/Components/TopThree.js
@@ -1,7 +1,16 @@
+// This is just like the old todo list app lets try not to make this too complicated...
+
+/*  The flow of the operations
+
+I - Get an input (Make an input box)
+
+
+*/
+
 function TopThreeTodo(){
     return(
         <div id="top-three">
-            An Empty Div
+            <input value="The to do list text is coming from here" ></input>
         </div>
     )
 }
