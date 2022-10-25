ea04dcaa3805a0e2fbf4a35540d05b7065fe2aec
Jordan <JMCollins0@aol.com>
2022-10-25 11:49:13 -0400

    add input box state can listen to input

--- src/Components/TopThree.js
+++ src/Components/TopThree.js
@@ -3,14 +3,27 @@ class TopThreeTODO extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-
+      ToDoInput: "I'm jordan",
+      ListElements: ["Hello", "World"]
     }
+
+    this.testAdd = this.testAdd.bind(this);
+    this.updateTextInput = this.updateTextInput.bind(this);
   }
-compoentn
+
+  updateTextInput(e){
+    this.setState({ToDoInput: e.target.value})
+  }
+
+  testAdd(){
+  this.setState({ListElements: this.state.ListElements.concat(<li>{this.state.otherState}</li>)})
+  }
   render() {
     return (
       <div id="top">
-        Anything at all
+        <ul>{this.state.ListElements}</ul>
+        <input value={this.state.ToDoInput} type="text" onChange={this.updateTextInput}></input>
+        <button onClick={this.testAdd}>Test Add</button>
       </div>
     )
   }
