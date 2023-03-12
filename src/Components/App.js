5fa8350a88100658fd9efe4e555dfcdd1e04531c
Jordan <JMCollins0@aol.com>
2023-03-12 16:16:36 -0400

    Remove useless TODO mini component.

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,12 +3,10 @@ import "../Styles/App.css";
 import "../Styles/topthree.css";
 import { AllLinks } from "./links.js";
 import Clock from "./clock.js";
-import TopThreeTODO from "./TopThree";
 function App() {
   return (
     <div id="app">
       <Clock />
-      <TopThreeTODO/>
       <AllLinks />
     </div>
   );
